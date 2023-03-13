569eb923bd36b5554f7077c039f217d3448f071c
user <ashirova158@gmail.com>
2023-03-13 16:29:02 +0600

    task 5

--- oop/mistakes_intro_oop.py
+++ oop/mistakes_intro_oop.py
@@ -70,4 +70,8 @@ class Phone:
     def __init__(self, name, last_name, phone):
         self.name = name
         self.last_name = last_name
-        self.cost_km = cost_km
+        self.phone = phone
+    def get_info(self):
+        print(f'Контакт: {self.name} {self.last_name}, телефон: {self.phone}')
+contact = Phone('John', 'Snow', '996707707707')
+contact.get_info()
