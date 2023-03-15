ea05f82a0ded6fd08457ad5a42833eee3d4c593f
user <ashirova158@gmail.com>
2023-03-15 23:16:47 +0600

    4 task

--- oop/maistakes_inheritance.py
+++ oop/maistakes_inheritance.py
@@ -18,3 +18,15 @@ example  =  MyString('String')
 print(example.append('hello'))
 print(example.pop()) 
 print(example)
+
+# Task 4
+class MyDict(dict):
+    def __init__(self, dict_):
+        self.dict_ = dict_
+    def get(self, key):
+        if self.dict_.get(key) == None:
+            return 'Are you kidding?'
+        return self.dict_.get(key)
+
+obj_dict = MyDict({'some_title': 2}) 
+print(obj_dict.get('some')) 
