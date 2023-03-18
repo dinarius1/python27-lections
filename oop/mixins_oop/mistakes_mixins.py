71d4f47c94ea85718a7a87a5a30b2b3e21e34735
user <ashirova158@gmail.com>
2023-03-18 16:30:59 +0600

    4 task

--- oop/mixins_oop/mistakes_mixins.py
+++ oop/mixins_oop/mistakes_mixins.py
@@ -64,3 +64,88 @@ class AlarmClock(Clock, Alarm):
 clock = AlarmClock()
 clock.current_time
 clock.alarm_on() 
+
+'''
+Задание 4
+
+Напишите абстрактный класс Coder с атрибутом класса count_code_time = 0 и абстрактными методами get_info и coding.
+
+Создайте классы Backend и Frontend, которые наследуют все атрибуты и методы от класса Coder.
+
+Класс Backend должен принимать дополнительно параметры experience и languages_backend, а Frontend такие параметры как — experience и languages_frontend соответственно.
+
+Переопределите в обоих классах методы get_info и coding так, чтобы он принимал количество часов кодинга и при каждом вызове этого метода добавлял это значение к count_code_time.
+
+Также бывают Fullstack разработчики, поэтому создайте данный класс так чтобы у него были атрибуты и методы предыдущих классов. При этом нее определяйте никаких методов и атрибутов в данном классе он должен наследовать их от родительских классов.
+
+Создайте экземпляры a, b, c от классов Backend, Frontend, Fullstack соответственно и вызовите их методы.
+
+Ввод должен быть:
+
+a.coding(12) 
+b.coding(45) 
+c.coding(17) 
+print(a.get_info()) 
+print(b.get_info()) 
+print(c.get_info()) 
+
+Вывод:
+
+Python разработчик, уровень: Junior, потрачено 12 часов на программирование
+
+Javascript разработчик, уровень: Middle, потрачено 45 часов на программирование
+
+Python and JS разработчик, уровень: Senior, потрачено 17 часов на программирование 
+'''
+from abc import ABC, abstractmethod
+class AbstractCoder(ABC):
+    count_code_time = 0
+    @abstractmethod
+    def get_info():
+        pass
+
+    @abstractmethod
+    def coding():
+        pass
+
+class Backend(AbstractCoder):
+    def __init__(self, languages_backend, experience):
+        self.experience = experience
+        self.languages_backend = languages_backend
+        
+    def coding(self, count_code_time):
+        self.count_code_time += count_code_time
+        return self.count_code_time
+    def get_info(self):
+        return f'{self.languages_backend} разработчик, уровень: {self.experience}, потрачено {self.count_code_time} часов на программирование'
+
+class Frontend(AbstractCoder):
+    def __init__(self, languages_frontend, experience):
+        self.experience = experience
+        self.languages_frontend = languages_frontend 
+    
+    def coding(self, count_code_time):
+        self.count_code_time += count_code_time
+        return self.count_code_time
+    
+    def get_info(self):
+        return f'{self.languages_frontend} разработчик, уровень: {self.experience}, потрачено {self.count_code_time} часов на программирование'
+
+class Fullstack(Frontend, Backend):
+    def __init__(self, languages_frontend, experience):
+        super().__init__(languages_frontend, experience)
+    
+    def coding(self, count_code_time):
+        return super().coding(count_code_time)
+    
+a = Backend('Python', 'Junior')
+b = Frontend('Javascript', 'Middle')
+c = Fullstack('Python and JS разработчик', 'Senior')
+
+a.coding(12) 
+b.coding(45) 
+c.coding(17) 
+print(a.get_info()) 
+print(b.get_info()) 
+print(c.get_info()) 
+
