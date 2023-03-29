05e2b17f4ca9cc7dfcf5fa5e75cbb3ca36bad6de
user <ashirova158@gmail.com>
2023-03-29 23:01:17 +0600

    5 task

--- psql/rno.sql
+++ psql/rno.sql
@@ -9,3 +9,9 @@ select title, genretype from work where genretype = 'p';
 
 -- 4 task
 select avg(totalparagraphs) as avg from work where genretype = 't';
+
+-- 5 task
+select avg(totalwords) as avg from work;
+select title from work where totalwords > 20563;
+
+-- 9 task
