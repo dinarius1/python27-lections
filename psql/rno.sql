9d2bbaa1694fc9e09ee394dcae17cebab78f6e97
user <ashirova158@gmail.com>
2023-03-30 15:56:16 +0600

    6 task

--- psql/rno.sql
+++ psql/rno.sql
@@ -14,6 +14,26 @@ select avg(totalparagraphs) as avg from work where genretype = 't';
 select avg(totalwords) as avg from work;
 select title from work where totalwords > 20563;
 
+-- 6 task
+Выведите имя героя, количество его реплик и название произведения, в котором этот герой встречается(таблицы character_work и character).
+
+Ожидаемый вывод:
+
+   charname         | speechcount |     title            
+--------------------+-------------+------------------------ 
+First Apparition    |           1 | Macbeth 
+First Citizen       |           3 | Romeo and Juliet 
+First Conspirator   |           3 | Coriolanus 
+First Gentleman     |           1 | Othello 
+First Goth          |           4 | Titus Andronicus 
+... 
+(1346 row) 
+
+select character.charname, character.speechcount, work.title 
+from character join character_work on character.charid = character_work.charid join work on character_work.workid = work.workid;
+
+
+
 -- 9 task
 select charname, speechcount from character where speechcount >= 15 and speechcount <= 30;
 
